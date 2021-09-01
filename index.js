3a056f06de3e48e3f7004ac63fe1d39fed9642ce
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-01 08:18:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const n = new Telegraf(process.env.token)
 n.start((ctx) => ctx.reply('Hello! Im remover tag bot. Fordward a message'))
 
 n.on('message', (ctx) => {
-    ctx.copyMessage(ctx.message.chat.id)
+    ctx.copyMessage(process.env.chat_id)
    })
 n.launch()

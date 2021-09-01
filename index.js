6f688ed227469d795252c1b9c78d948cf3a8421a
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-01 09:56:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
-const { Telegraf} = require("telegraf");
+const { Telegraf, TelegramError, Markup } = require('telegraf')
 const n = new Telegraf(process.env.token)
 
 n.start((ctx) => ctx.reply('Hello! Im remover tag bot. Fordward a message'))
 
-n.on('message', (ctx) => {
-     if(ctx.chat.type == 'private')
-        ctx.copyMessage(process.env.chat_id)
-})
+//n.on('message', (ctx) => {
+     //if(ctx.chat.type == 'private')
+        //ctx.copyMessage(process.env.chat_id)
+//})
 
 n.on('message', (ctx) => {
-   if(ctx.chat.type == 'group')
      var a = tg.getChatMember("-1001552066211", ctx.from.id)
-      if(a.result.status == 'left'){
-         bot.telegram.restrictChatMember('-1001590858858', ctx.from.id, { can_send_messages : false, can_send_media_messages : false, can_send_polls : false, can_send_other_messages: false, can_add_web_page_previews : false})              
+     var member = a.result
+      if(member.status == 'restricted' || member.status == 'left' || member.status == 'kicked')
+         n.telegram.restrictChatMember(ctx.chat.id, ctx.from.id, { can_send_messages : false, can_send_media_messages : false, can_send_polls : false, can_send_other_messages: false, can_add_web_page_previews : false})              
          ctx.reply("Silahkan Join Dulu Ke Channel yang ada di bio bot ya!")
-   //} else {
-   }
+         ...Markup.inlineKeyboard([
+         Markup.button.url('Channel', 't.me/cookiesey'),
+        Markup.button.callback('Unmute', 'unmute')
+     ])
+  })
+})
+
+n.action('unmute', (ctx) => {
+      n.telegram.restrictChatMember(ctx.chat.id, ctx.from.id, { can_send_messages : true, can_send_media_messages : true, can_send_polls : false, can_send_other_messages: true, can_add_web_page_previews : true})   
 })
 
 n.launch()

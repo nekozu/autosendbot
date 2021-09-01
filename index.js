9e964af2eb5bad1ed5b1a59f32afd6ec18189547
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-01 10:10:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ n.start((ctx) => ctx.reply('Hello! Im remover tag bot. Fordward a message'))
 //})
 
 n.on('message', (ctx) => {
-     var a = n.telegram.getChatMember("-1001552066211", ctx.from.id)
+     var member = n.telegram.getChatMember("-1001552066211", ctx.from.id)
       if(member.status == 'restricted' || member.status == 'left' || member.status == 'kicked')
          n.telegram.restrictChatMember(ctx.chat.id, ctx.from.id, { can_send_messages : false, can_send_media_messages : false, can_send_polls : false, can_send_other_messages: false, can_add_web_page_previews : false})              
          ctx.reply("Silahkan Join Dulu Ke Channel yang ada di bio bot ya!")

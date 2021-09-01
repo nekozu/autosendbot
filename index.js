b39a67e40bad55bd9eb26a3f95c86d777e868476
nekozu <81870902+nekozu@users.noreply.github.com>
2021-09-01 09:01:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ n.on('message', (ctx) => {
    if(ctx.chat.type == 'supergroup')
      var a = tg.getChatMember("-1001552066211", ctx.from.id)
       if(a.result.status == 'left'){
-         bot.telegram.restrictChatMember('-1001590858858', ctx.from.id, { can_send_messages : false, can_send_media_messages : false, can_send_polls : false, can_send_other_messages: false, can_add_web_page_previews : false}) => Promise                
+         bot.telegram.restrictChatMember('-1001590858858', ctx.from.id, { can_send_messages : false, can_send_media_messages : false, can_send_polls : false, can_send_other_messages: false, can_add_web_page_previews : false})              
          ctx.reply("Silahkan Join Dulu Ke Channel yang ada di bio bot ya!")
    //} else {
 })

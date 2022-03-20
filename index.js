2fd48232c214c7227bb0a932150c07c87bd1b1e9
scer <81870902+scerviez@users.noreply.github.com>
2022-03-20 14:25:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const n = new Telegraf(process.env.token)
 n.start((ctx) => ctx.reply('kirim pesanmu'))
 
 n.on('message', (ctx) => {
-     if(ctx.chat.type == 'private')
-        ctx.copyMessage(process.env.chat_id)
+   const from_id = ctx.msg.from.id;
+    if (from_id == own_id){
+        ;
+    } else {
+        const msg_id = ctx.msg.message_id;
+        await ctx.api.forwardMessage(process.env.group_id, from_id, msg_id);
+    }
+    const reply_msg = ctx.update.message.reply_to_message
+    const reply_msg_user_id = reply_msg.forward_from.id
+    const own_msg = ctx.update.message.text
+    if (reply_msg){
+        await ctx.api.sendMessage(reply_msg_user_id, own_msg)
+    }
 })
 
 n.launch()
+

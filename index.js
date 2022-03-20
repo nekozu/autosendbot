48e086038455676abe8d41df4361b31785e87e0b
scer <81870902+scerviez@users.noreply.github.com>
2022-03-20 05:02:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ n.on('message', (ctx) => {
     ctx.forwardMessage(chat_id, msg, text);
     }
     const reply_msg = ctx.message.reply_to_message?.message_id
-    const reply_msg_user_id = reply_msg?.forward_from?.id
+    const reply_msg_user_id = reply_msg.forward_from.id
     console.log(reply_msg_user_id)
     const own_msg = ctx.message.text
     if (reply_msg){

fe85511d4f88b4fb8ae50304caad9872114e8335
scer <81870902+scerviez@users.noreply.github.com>
2022-03-20 01:48:20 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Telegraf} = require("telegraf");
 const n = new Telegraf(process.env.token)
+const chat_id = process.env_chat_id
 
 n.start((ctx) => ctx.reply('Hello! '))
 
 n.on('message', (ctx) => {
     let msg = ctx.message.from.id
     let text = ctx.message.message_id
-    let chat_id = process.env_chat_id
     ctx.forwardMessage(chat_id, msg, text);
     const reply_msg = ctx.message.reply_to_message
     const reply_msg_user_id = reply_msg.forward_from.id

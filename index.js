6ff90834d05715ecb63d0440b78cd59736d52839
scer <81870902+scerviez@users.noreply.github.com>
2022-03-20 05:06:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ n.on('message', (ctx) => {
     }
     const reply_msg = ctx.message.reply_to_message?.message_id
     const reply_msg_user_id = reply_msg.forward_from.id
-    console.log(reply_msg_user_id)
+    ctx.reply(reply_msg_user_id)
     const own_msg = ctx.message.text
-    if (reply_msg){
-         ctx.telegram.sendMessage(reply_msg_user_id, own_msg)
-    }
+    //if (reply_msg){
+         //ctx.telegram.sendMessage(reply_msg_user_id, own_msg)
+    //}
 })
 
 n.launch()

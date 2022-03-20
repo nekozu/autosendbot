e045c3a0812cc4c2b4fb92d4d1687ae38f8678b7
scer <81870902+scerviez@users.noreply.github.com>
2022-03-20 14:54:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const { Telegraf, TelegramError, Markup } = require('telegraf')
-const n = new Telegraf(process.env.token)
+const {Bot} = require('grammy');
+const {run} = require('@grammyjs/runner');
 
-n.start((ctx) => ctx.reply('kirim pesanmu'))
+const bot = new Bot(process.env.token);
+const own_id = process.env.chat_id;
 
-n.on('message', (ctx) => {
-   const from_id = ctx.msg.from.id;
+bot.command('start', async ctx => {
+    return await ctx.reply('halo silahkan kirim pesanmu');
+});
+
+bot.on('message', async ctx => {
+    const from_id = ctx.msg.from.id;
     if (from_id == own_id){
         ;
     } else {
         const msg_id = ctx.msg.message_id;
-        ctx.api.forwardMessage(process.env.chat_id, from_id, msg_id);
+        await ctx.api.forwardMessage(own_id, from_id, msg_id);
     }
     const reply_msg = ctx.update.message.reply_to_message
     const reply_msg_user_id = reply_msg.forward_from.id
     const own_msg = ctx.update.message.text
     if (reply_msg){
-        ctx.api.sendMessage(reply_msg_user_id, own_msg)
+        await ctx.api.sendMessage(reply_msg_user_id, own_msg)
     }
 })
 
-n.launch()
-
+run(bot);
